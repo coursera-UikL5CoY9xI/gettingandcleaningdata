6f8b45c26416c41aacd3896b89b8b4bd7d56fdd0
Erik Berls <cyber@koolaid.core.ono-sendai.com>
2015-10-25 00:04:45 -0700

    base tidy

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# gettingandcleaningdata
-Getting and cleaning data assignment
+# Getting and Cleaning Data 
+
+Starting from data sourced at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+data is tidy'ed and restored.
+
+The following controlled files make up the processing:
+* README.md      - This file
+* run_analysis.R - The core dataprocessing file
+* codebook.txt   - Explanation of data
+
+The program processing the tidying is run_analysis.R, which generates the following derrived files:
+* subject_labels.txt 
+* subject_data.txt
+* average_data.txt
+
+#### subject_labels.txt
+This file consists of two columns:
+* rowname - index (ID)
+* colName - descritive name of the values in the column
+
+#### subject_data.txt
+This is the tidyied data from the original source.
+
+
+#### average_data.txt
+This data is averages around each data category, processed by subject by activity.
+
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+# Data setup
+# We assume that the data has been downloaded (Dataset.zip) and extracted in this directory.
+# It should have a subdir of "UCI HAR Dataset"
+
+# Pull in activity lables so we can map
+activity_lables = read.table("UCI HAR Dataset/activity_labels.txt")
+colnames(activity_lables)  <- c("id", "activity")
+
+# Pull in information about column naming
+features_src  <- read.table("UCI HAR Dataset/features.txt")
+# reduce to table that only has -mean() and -std() entries in V2
+# this will be used to reduce dataset to only mean and std deviation, below
+features  <- features_src[features_src$V2 %like% "-mean\\()" | features_src$V2 %like% "-std\\()",]
+rm(features_src)
+# V1 becomes the columns we want to keep, V2 becomes the colNames of the new table
+colIndex  <- features$V1
+features[]  <- lapply(features, as.character) # de-factorize the column names
+colNames  <- c("subject", "activity", features$V2)
+newmap  <- data.frame(colNames)
+rm(colNames)
+# Write out new schema column naming.
+write.table(newmap, file="subject_labels.txt", col.names = FALSE, quote=FALSE)
+
+loadData  <- function(event) {
+    X_file  <- paste("UCI HAR Dataset/", event, "/X_", event, ".txt", sep="")
+    Y_file  <- paste("UCI HAR Dataset/", event, "/Y_", event, ".txt", sep="")
+    subject_file  <- paste("UCI HAR Dataset/", event, "/subject_", event, ".txt", sep="")
+
+    X  <- read.table(X_file)
+    Y  <- read.table(Y_file)
+    subj  <- read.table(subject_file)
+
+    # 2. Extract only the measurements on the mean and standard deviation for each measurement.
+    t  <- X[,colIndex]
+    # 3. Use descriptive activity names to name the activities in the data set.
+    Y  <- merge(Y, activity_lables, by.x = "V1", by.y="id")
+    Y$V1  <- NULL
+    t  <- cbind(subj, Y, t)
+    #  4. Appropriately label the data set with descriptive variable names.
+    colnames(t)  <- newmap$colNames
+    return(t)
+}
+
+# 1. Merge the training and the test sets to create one data set.
+TEST  <- loadData("test")
+TRAIN  <- loadData("train")
+SD  <- rbind(TEST, TRAIN); rm(TEST, TRAIN) # combine sets, remove temporaries
+SD  <- SD[order(SD$subject, SD$activity),] # reorder by subject, then activity
+rownames(SD)  <- seq_along(1:nrow(SD))  # reset the rownames to be an ordered numbered list
+
+# 5. From the data set above, create a second, independent tidy data set with the average of
+# each variable for each activity and each subject.
+
+
+## Output the final results
+write.table(SD, file="subject_data.txt", row.names=FALSE)
+#write.table(AD, file="average_data.txt", row.names=FALSE)
+
